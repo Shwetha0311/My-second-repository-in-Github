0ab78147b4612ee19ab528f78e1680cfa8481439
pavan <pavan@Shwetha>
2022-07-26 17:13:11 -0400

    Modified Code

--- src/Practice2/SimpleCode.java
+++ src/Practice2/SimpleCode.java
@@ -3,7 +3,7 @@ package Practice2;
 public class SimpleCode {
 
 	public static void main(String[] args) {
-System.out.println("Exporting this code to Github");
+System.out.println("Pushing this code to Github");
 System.out.println("Making Changes");
 	}
 
