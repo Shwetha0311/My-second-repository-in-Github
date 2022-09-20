cff0b1a9765f7e8e94d11472e3cd19ea37b5235c
Shwetha <shwetha.311nov@gmail.com>
2022-09-20 16:30:51 -0400

    updateing offer

--- src/Practice2/SimpleCode.java
+++ src/Practice2/SimpleCode.java
@@ -6,6 +6,8 @@ public class SimpleCode {
 System.out.println("Pushing this code to Github");
 System.out.println("Making Changes");
 System.out.println("Testing code on jenkins");
+
+System.out.println("Offer of the day 20% discount on iPhone 13 Pro");
 	}
 
 }
