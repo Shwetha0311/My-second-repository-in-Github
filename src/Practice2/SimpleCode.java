0d21072521637e18b94c66d7c2f382d16eab1bfc
Shwetha <shwetha.311nov@gmail.com>
2022-09-20 16:28:46 -0400

    Comitting the code.

--- src/Practice2/SimpleCode.java
+++ src/Practice2/SimpleCode.java
@@ -5,6 +5,7 @@ public class SimpleCode {
 	public static void main(String[] args) {
 System.out.println("Pushing this code to Github");
 System.out.println("Making Changes");
+System.out.println("Testing code on jenkins");
 	}
 
 }
